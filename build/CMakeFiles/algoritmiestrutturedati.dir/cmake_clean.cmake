b906dd4d257623781d373b2c9cd88653a649df84
Enrycoop <enrycoop92@gmail.com>
2022-10-24 17:30:05 +0200

    liste aggiunte

---
+++ build/CMakeFiles/algoritmiestrutturedati.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algoritmiestrutturedati.dir/lista.cpp.o"
+  "CMakeFiles/algoritmiestrutturedati.dir/main.cpp.o"
+  "algoritmiestrutturedati"
+  "algoritmiestrutturedati.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algoritmiestrutturedati.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
