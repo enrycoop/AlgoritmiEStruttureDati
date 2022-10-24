fb5948139915a176df00d69ff3707218a86b3ab3
Enrycoop <enrycoop92@gmail.com>
2022-10-24 12:37:41 +0200

    Start Lists

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "lista.h"
 
 int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
+    Lista<int> a = Lista<int>();
+    Lista<int>::Pos t = a.head();
+    for (int i = 0; i<10; i++) {
+        t = a.insert(t,i);
+    }
+    a.print();
+        
     return 0;
 }
